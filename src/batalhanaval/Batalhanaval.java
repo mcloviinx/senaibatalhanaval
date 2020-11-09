c74a53179bc94d3ddf3c74d7fb8837ac4d36c0d1
Leonardo Santos <sleonardo144@gmail.com>
2020-11-09 20:31:07 -0200

    fala caio

--- src/batalhanaval/Batalhanaval.java
+++ src/batalhanaval/Batalhanaval.java
@@ -3,8 +3,7 @@ package batalhanaval;
 public class Batalhanaval {
 
     public static void main(String[] args) {
-        System.out.println("ola");
-        //testando git
+        // olá caio fala tu!
     }
     
 }
