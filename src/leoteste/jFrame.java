8f48769199cf1ae6d3dac6127c51f3641abd2907
Leonardo Santos <sleonardo144@gmail.com>
2020-11-09 23:11:45 -0200

    exemplo com array

--- src/leoteste/jFrame.java
+++ src/leoteste/jFrame.java
@@ -2,8 +2,36 @@
 package leoteste;
 
 import batalhanaval.Botao;
+import java.awt.Container;
+import java.awt.GridLayout;
 import javax.swing.JFrame;
 
 public class jFrame extends JFrame{
+    int numColunas = 5;
+    int numLinhas = 5;
+    Botao[][] botao = new Botao[numLinhas][numColunas];
     
+    public jFrame(){
+        iniciarBotoes();
+        iniciarFrame();
+    }
+    private void iniciarFrame(){
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container painel = getContentPane();
+        painel.setLayout(new GridLayout(numLinhas, numColunas));
+        for (int y = 0; y < numColunas; y++){
+            for (int x = 0; x < numLinhas; x++){
+                painel.add(botao[x][y]);
+            }
+        }
+        pack();
+        setVisible(true);
+    }
+    private void iniciarBotoes(){
+        for (int y = 0; y < numColunas; y++){
+            for (int x = 0; x < numLinhas; x++){
+                botao[x][y] = new Botao();
+            }
+        }
+    }
 }
