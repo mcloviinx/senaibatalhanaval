77d989705959603be11ae0141433832f625fa450
Leonardo Santos <sleonardo144@gmail.com>
2020-11-09 22:20:55 -0200

    configurando botão

--- src/batalhanaval/Botao.java
+++ src/batalhanaval/Botao.java
@@ -1,10 +1,28 @@
 package batalhanaval;
 
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import javax.swing.JButton;
 
 public class Botao extends JButton{
-    
     public Botao(){
         
+        initButton();        
+    }   
+    
+    private void initButton() {
+        this.setPreferredSize(new Dimension(25, 25));
+        this.setFont(new Font("Calibri", 1, 25));
+        this.setFocusPainted(false);        
+        this.setBackground(Color.black);        
+        this.addMouseListener(new MouseAdapter(){            
+            @Override            
+            public void mousePressed(MouseEvent e){
+                
+            }            
+        });
     }
 }
