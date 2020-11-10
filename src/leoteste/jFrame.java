b93468fdf8080298f383c7b0b903fc9da6312072
Leonardo Santos <sleonardo144@gmail.com>
2020-11-10 10:47:23 -0200

    colocando os paineis no jframe

--- src/leoteste/jFrame.java
+++ src/leoteste/jFrame.java
@@ -1,37 +1,30 @@
 
 package leoteste;
 
-import batalhanaval.Botao;
+import batalhanaval.*;
+import java.awt.BorderLayout;
 import java.awt.Container;
-import java.awt.GridLayout;
 import javax.swing.JFrame;
 
 public class jFrame extends JFrame{
-    int numColunas = 5;
-    int numLinhas = 5;
-    Botao[][] botao = new Botao[numLinhas][numColunas];
     
     public jFrame(){
-        iniciarBotoes();
         iniciarFrame();
     }
     private void iniciarFrame(){
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Container painel = getContentPane();
-        painel.setLayout(new GridLayout(numLinhas, numColunas));
-        for (int y = 0; y < numColunas; y++){
-            for (int x = 0; x < numLinhas; x++){
-                painel.add(botao[x][y]);
-            }
-        }
-        pack();
-        setVisible(true);
-    }
-    private void iniciarBotoes(){
-        for (int y = 0; y < numColunas; y++){
-            for (int x = 0; x < numLinhas; x++){
-                botao[x][y] = new Botao();
-            }
-        }
+        painel.setLayout(new BorderLayout()); //definindo layout de borda
+        
+        // criando 2 paineis do mesmo objeto
+        jogadorPanel jogador1 = new jogadorPanel(); //botoes do jogador1
+        jogadorPanel jogador2 = new jogadorPanel(); //botoes do jogador2
+        
+        painel.add(jogador1, BorderLayout.EAST); //adicionando o painel do jogador1 no lest(direita) 
+        painel.add(jogador2, BorderLayout.WEST); //adicionando o painel do jogador1 no oeste(esquerda)
+        
+        pack(); //esse metodo organiza automaticamente os espaços de cada elemento do jframe;
+        setLocationRelativeTo(null); //exibe a janela no meio da tela
+        setVisible(true); //exibe o jframe para o usuario;
     }
 }
