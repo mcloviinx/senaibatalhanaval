edb3835aec638fbc10e37ec748742e7dfde9b524
Leonardo Santos <sleonardo144@gmail.com>
2020-11-11 18:54:04 -0200

    tentativa de fazer um gerador de navios

---
+++ src/batalhanaval/Gerador.java
@@ -0,0 +1,32 @@
+
+package batalhanaval;
+
+
+public class Gerador {
+    
+    public Gerador(){
+        
+    }
+    static int[] getNavio(String tipo, int gridX, int gridY){
+        int[] values = new int[2];
+        
+        switch (tipo) {
+            default: 
+                
+            break;
+            case "submarino":
+                
+            break;
+            case "cruzador":
+                
+            break;
+            case "encouracado":
+                
+            break;
+            case "partaavioes":
+                
+            break;
+        }
+        return values;
+    }
+}
