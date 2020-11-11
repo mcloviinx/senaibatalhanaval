a04ac20292c03031f491899e0c0c2930616132a4
Leonardo Santos <sleonardo144@gmail.com>
2020-11-11 18:54:28 -0200

    alterando algumas propriedades do jbutton

--- src/batalhanaval/Botao.java
+++ src/batalhanaval/Botao.java
@@ -5,23 +5,47 @@ import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 public class Botao extends JButton{
+    boolean navio = false;
+    boolean clicado = false;
+    private final ImageIcon miraIcon = new ImageIcon(Class.class.getResource("/png/mira25.png"));
+    private final ImageIcon erroIcon = new ImageIcon(Class.class.getResource("/png/erro25.png"));
+    
     public Botao(){
         initButton();        
     }   
     private void initButton() {
-        this.setPreferredSize(new Dimension(75, 75));
+        this.setPreferredSize(new Dimension(45, 45));
         this.setFont(new Font("Calibri", 1, 25));
         this.setFocusPainted(false);        
-        this.setBackground(Color.black);
+        this.setBackground(new Color(0, 105, 148));
+        this.setBorder(BorderFactory.createEtchedBorder());
         this.addMouseListener(new MouseAdapter(){            
             @Override            
             public void mousePressed(MouseEvent e){
-                
-            }            
+                clicado = true;
+                if (navio){
+                    
+                } else {              
+                    setIcon(erroIcon);
+                }
+            }
+            @Override
+            public void mouseEntered(MouseEvent e){
+                if (!clicado){
+                    setIcon(miraIcon);
+                }
+            }
+            @Override
+            public void mouseExited(MouseEvent e){
+                if (!clicado){
+                    setIcon(null);
+                }
+            }
         });
-        
     }
 }
