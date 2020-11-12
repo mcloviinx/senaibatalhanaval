c2139627454496f31b243705e982e1b390b00170
Leonardo Santos <sleonardo144@gmail.com>
2020-11-12 12:40:39 -0200

    implementado alternação

--- src/batalhanaval/Frame.java
+++ src/batalhanaval/Frame.java
@@ -6,22 +6,30 @@ import java.awt.Container;
 import javax.swing.JFrame;
 
 public class Frame extends JFrame{
+    boolean vezjogador = true;
+    jogadorPanel jogador1, jogador2;
     public Frame() {
         iniciarJanela();
     }
     private void iniciarJanela(){
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Container painel = getContentPane();
-        
         painel.setLayout(new BorderLayout());
-        jogadorPanel jogador1 = new jogadorPanel();
-        jogadorPanel jogador2 = new jogadorPanel();
+        jogador1 = new jogadorPanel(this);
+        jogador2 = new jogadorPanel(this);
+        jogador1.setPanelEnabled(vezjogador);
+        jogador2.setPanelEnabled(!vezjogador);
         painel.add(jogador1, BorderLayout.WEST);
         painel.add(jogador2, BorderLayout.EAST);
-        painel.add(new Infor(), BorderLayout.CENTER);
-        
+        Infor infor = new Infor();
+        painel.add(infor, BorderLayout.CENTER);
         pack();
         setExtendedState(MAXIMIZED_BOTH);
         setVisible(true);
+    }
+    public void updateVezJogador(){
+        vezjogador = !vezjogador;
+        jogador1.setPanelEnabled(vezjogador);
+        jogador2.setPanelEnabled(!vezjogador);
     }
 }
