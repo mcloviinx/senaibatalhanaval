f89e604d6002b28f3752aae8f9c81795d8cbf81a
Leonardo Santos <sleonardo144@gmail.com>
2020-11-12 17:55:52 -0200

    .

--- src/batalhanaval/Infor.java
+++ src/batalhanaval/Infor.java
@@ -5,6 +5,10 @@
  */
 package batalhanaval;
 
+import java.awt.Color;
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
+
 /**
  *
  * @author Caio Gabriel
@@ -16,6 +20,7 @@ public class Infor extends javax.swing.JPanel {
      */
     public Infor() {
         initComponents();
+        this.setBorder(BorderFactory.createMatteBorder(3, 0, 3, 0, Color.black));
     }
     
     /**
@@ -44,12 +49,20 @@ public class Infor extends javax.swing.JPanel {
         button9 = new java.awt.Button();
         button10 = new java.awt.Button();
         button12 = new java.awt.Button();
+        button11 = new java.awt.Button();
+        button7 = new java.awt.Button();
         jPanel2 = new javax.swing.JPanel();
         jLabel7 = new javax.swing.JLabel();
         vtr_1 = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
+        jLabel10 = new javax.swing.JLabel();
+        pplayer2 = new javax.swing.JLabel();
         jPanel3 = new javax.swing.JPanel();
-        jLabel8 = new javax.swing.JLabel();
-        vtr_2 = new javax.swing.JLabel();
+        jLabel12 = new javax.swing.JLabel();
+        vtr_3 = new javax.swing.JLabel();
+        jLabel13 = new javax.swing.JLabel();
+        jLabel14 = new javax.swing.JLabel();
+        pplayer1 = new javax.swing.JLabel();
         jPanel4 = new javax.swing.JPanel();
         bt_novojg = new javax.swing.JButton();
         bt_sairjg = new javax.swing.JButton();
@@ -58,7 +71,6 @@ public class Infor extends javax.swing.JPanel {
         setLayout(new java.awt.GridLayout(4, 0));
 
         jPanel1.setBackground(new java.awt.Color(102, 102, 102));
-        jPanel1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
         jPanel1.setLayout(new java.awt.GridBagLayout());
 
         jLabel1.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
@@ -72,7 +84,7 @@ public class Infor extends javax.swing.JPanel {
         gridBagConstraints.insets = new java.awt.Insets(6, 16, 12, 57);
         jPanel1.add(jLabel1, gridBagConstraints);
 
-        jLabel3.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        jLabel3.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
         jLabel3.setForeground(new java.awt.Color(255, 255, 255));
         jLabel3.setText("Submarino");
         gridBagConstraints = new java.awt.GridBagConstraints();
@@ -82,7 +94,7 @@ public class Infor extends javax.swing.JPanel {
         gridBagConstraints.insets = new java.awt.Insets(4, 18, 6, 31);
         jPanel1.add(jLabel3, gridBagConstraints);
 
-        jLabel4.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        jLabel4.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
         jLabel4.setForeground(new java.awt.Color(255, 255, 255));
         jLabel4.setText("Cruzador");
         gridBagConstraints = new java.awt.GridBagConstraints();
@@ -93,7 +105,7 @@ public class Infor extends javax.swing.JPanel {
         jPanel1.add(jLabel4, gridBagConstraints);
 
         jLabel5.setBackground(new java.awt.Color(255, 255, 255));
-        jLabel5.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        jLabel5.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
         jLabel5.setForeground(new java.awt.Color(255, 255, 255));
         jLabel5.setText("Encouraçado");
         gridBagConstraints = new java.awt.GridBagConstraints();
@@ -103,7 +115,7 @@ public class Infor extends javax.swing.JPanel {
         gridBagConstraints.insets = new java.awt.Insets(4, 7, 6, 0);
         jPanel1.add(jLabel5, gridBagConstraints);
 
-        jLabel6.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        jLabel6.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
         jLabel6.setForeground(new java.awt.Color(255, 255, 255));
         jLabel6.setText("Porta-Aviões");
         gridBagConstraints = new java.awt.GridBagConstraints();
@@ -154,9 +166,8 @@ public class Infor extends javax.swing.JPanel {
         button4.setForeground(new java.awt.Color(204, 0, 0));
         button4.setPreferredSize(new java.awt.Dimension(20, 20));
         gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 8;
+        gridBagConstraints.gridx = 9;
         gridBagConstraints.gridy = 1;
-        gridBagConstraints.ipadx = 4;
         gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
         gridBagConstraints.insets = new java.awt.Insets(0, 0, 6, 0);
         jPanel1.add(button4, gridBagConstraints);
@@ -194,10 +205,8 @@ public class Infor extends javax.swing.JPanel {
         button8.setForeground(new java.awt.Color(204, 0, 0));
         button8.setPreferredSize(new java.awt.Dimension(20, 20));
         gridBagConstraints = new java.awt.GridBagConstraints();
-        gridBagConstraints.gridx = 9;
+        gridBagConstraints.gridx = 10;
         gridBagConstraints.gridy = 2;
-        gridBagConstraints.ipadx = 4;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
         jPanel1.add(button8, gridBagConstraints);
 
         button9.setBackground(new java.awt.Color(0, 0, 153));
@@ -238,71 +247,169 @@ public class Infor extends javax.swing.JPanel {
         gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
         jPanel1.add(button12, gridBagConstraints);
 
+        button11.setBackground(new java.awt.Color(0, 0, 153));
+        button11.setEnabled(false);
+        button11.setForeground(new java.awt.Color(204, 0, 0));
+        button11.setPreferredSize(new java.awt.Dimension(20, 20));
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 9;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.ipadx = 4;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
+        jPanel1.add(button11, gridBagConstraints);
+
+        button7.setBackground(new java.awt.Color(204, 0, 0));
+        button7.setEnabled(false);
+        button7.setForeground(new java.awt.Color(204, 0, 0));
+        button7.setPreferredSize(new java.awt.Dimension(20, 20));
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 8;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.ipadx = 4;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
+        gridBagConstraints.insets = new java.awt.Insets(0, 0, 6, 0);
+        jPanel1.add(button7, gridBagConstraints);
+
         add(jPanel1);
         jPanel1.getAccessibleContext().setAccessibleName("Informações do Jogo");
         jPanel1.getAccessibleContext().setAccessibleDescription("");
 
         jPanel2.setBackground(new java.awt.Color(204, 204, 204));
-        jPanel2.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
         jPanel2.setLayout(new java.awt.GridBagLayout());
 
-        jLabel7.setBackground(new java.awt.Color(255, 255, 255));
-        jLabel7.setFont(new java.awt.Font("Times New Roman", 1, 12)); // NOI18N
-        jLabel7.setText("Jogador: 1     ");
+        jLabel7.setBackground(new java.awt.Color(0, 0, 255));
+        jLabel7.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        jLabel7.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel7.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel7.setText("JOGADOR 1");
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 0;
+        gridBagConstraints.gridwidth = 2;
         gridBagConstraints.ipady = 6;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-        gridBagConstraints.insets = new java.awt.Insets(44, 50, 11, 5);
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
         jPanel2.add(jLabel7, gridBagConstraints);
 
-        vtr_1.setFont(new java.awt.Font("Times New Roman", 1, 12)); // NOI18N
-        vtr_1.setText("Vitórias: 0");
+        vtr_1.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        vtr_1.setForeground(new java.awt.Color(0, 0, 0));
+        vtr_1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        vtr_1.setText("VITÓRIAS:");
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 1;
-        gridBagConstraints.ipadx = 35;
+        gridBagConstraints.ipadx = 10;
         gridBagConstraints.ipady = 6;
-        gridBagConstraints.insets = new java.awt.Insets(44, 50, 11, 5);
+        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
         jPanel2.add(vtr_1, gridBagConstraints);
+
+        jLabel9.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        jLabel9.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel9.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel9.setText("0");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.ipadx = 10;
+        gridBagConstraints.ipady = 6;
+        gridBagConstraints.insets = new java.awt.Insets(0, 5, 0, 5);
+        jPanel2.add(jLabel9, gridBagConstraints);
+
+        jLabel10.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        jLabel10.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel10.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel10.setText("DESTRUIÇÃO: ");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.ipady = 6;
+        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
+        jPanel2.add(jLabel10, gridBagConstraints);
+
+        pplayer2.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        pplayer2.setForeground(new java.awt.Color(0, 0, 0));
+        pplayer2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        pplayer2.setText("0%");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.ipady = 6;
+        gridBagConstraints.insets = new java.awt.Insets(0, 5, 0, 5);
+        jPanel2.add(pplayer2, gridBagConstraints);
 
         add(jPanel2);
 
         jPanel3.setBackground(new java.awt.Color(204, 204, 204));
-        jPanel3.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
         jPanel3.setLayout(new java.awt.GridBagLayout());
 
-        jLabel8.setBackground(new java.awt.Color(255, 255, 255));
-        jLabel8.setFont(new java.awt.Font("Times New Roman", 1, 12)); // NOI18N
-        jLabel8.setText("Jogador: 2");
+        jLabel12.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        jLabel12.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel12.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel12.setText("DESTRUIÇÃO: ");
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 0;
+        gridBagConstraints.gridy = 2;
         gridBagConstraints.ipady = 6;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-        gridBagConstraints.insets = new java.awt.Insets(44, 50, 11, 5);
-        jPanel3.add(jLabel8, gridBagConstraints);
+        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
+        jPanel3.add(jLabel12, gridBagConstraints);
 
-        vtr_2.setFont(new java.awt.Font("Times New Roman", 1, 12)); // NOI18N
-        vtr_2.setText("Vitórias: 0");
+        vtr_3.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        vtr_3.setForeground(new java.awt.Color(0, 0, 0));
+        vtr_3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        vtr_3.setText("VITÓRIAS:");
         gridBagConstraints = new java.awt.GridBagConstraints();
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 1;
-        gridBagConstraints.ipadx = 35;
+        gridBagConstraints.ipadx = 10;
         gridBagConstraints.ipady = 6;
-        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTHWEST;
-        gridBagConstraints.insets = new java.awt.Insets(44, 50, 11, 5);
-        jPanel3.add(vtr_2, gridBagConstraints);
+        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
+        jPanel3.add(vtr_3, gridBagConstraints);
+
+        jLabel13.setBackground(new java.awt.Color(0, 0, 255));
+        jLabel13.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        jLabel13.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel13.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel13.setText("JOGADOR 2");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 0;
+        gridBagConstraints.gridwidth = 2;
+        gridBagConstraints.ipady = 6;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
+        jPanel3.add(jLabel13, gridBagConstraints);
+
+        jLabel14.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        jLabel14.setForeground(new java.awt.Color(0, 0, 0));
+        jLabel14.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel14.setText("0");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.ipadx = 10;
+        gridBagConstraints.ipady = 6;
+        gridBagConstraints.insets = new java.awt.Insets(0, 5, 0, 5);
+        jPanel3.add(jLabel14, gridBagConstraints);
+
+        pplayer1.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
+        pplayer1.setForeground(new java.awt.Color(0, 0, 0));
+        pplayer1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        pplayer1.setText("0%");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 1;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.ipady = 6;
+        gridBagConstraints.insets = new java.awt.Insets(0, 5, 0, 5);
+        jPanel3.add(pplayer1, gridBagConstraints);
 
         add(jPanel3);
 
         jPanel4.setBackground(new java.awt.Color(102, 102, 102));
-        jPanel4.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
         jPanel4.setLayout(new java.awt.GridBagLayout());
 
         bt_novojg.setBackground(new java.awt.Color(255, 255, 255));
-        bt_novojg.setFont(new java.awt.Font("Times New Roman", 2, 12)); // NOI18N
+        bt_novojg.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
+        bt_novojg.setForeground(new java.awt.Color(0, 0, 0));
         bt_novojg.setText("Novo Jogo");
         bt_novojg.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -316,7 +423,8 @@ public class Infor extends javax.swing.JPanel {
         jPanel4.add(bt_novojg, gridBagConstraints);
 
         bt_sairjg.setBackground(new java.awt.Color(255, 255, 255));
-        bt_sairjg.setFont(new java.awt.Font("Times New Roman", 2, 12)); // NOI18N
+        bt_sairjg.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
+        bt_sairjg.setForeground(new java.awt.Color(0, 0, 0));
         bt_sairjg.setText("Sair do Jogo");
         bt_sairjg.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -329,7 +437,7 @@ public class Infor extends javax.swing.JPanel {
         gridBagConstraints.insets = new java.awt.Insets(11, 0, 0, 0);
         jPanel4.add(bt_sairjg, gridBagConstraints);
 
-        jLabel2.setFont(new java.awt.Font("Times New Roman", 3, 14)); // NOI18N
+        jLabel2.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
         jLabel2.setForeground(new java.awt.Color(255, 255, 255));
         jLabel2.setText("OPÇÕES DO JOGO:");
         jPanel4.add(jLabel2, new java.awt.GridBagConstraints());
@@ -345,7 +453,7 @@ public class Infor extends javax.swing.JPanel {
 
     private void bt_sairjgActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_bt_sairjgActionPerformed
         // TODO add your handling code here:
-
+        System.exit(0);
     }//GEN-LAST:event_bt_sairjgActionPerformed
 
     private void button5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_button5ActionPerformed
@@ -356,33 +464,52 @@ public class Infor extends javax.swing.JPanel {
         // TODO add your handling code here:
     }//GEN-LAST:event_button10ActionPerformed
 
-
+    public void setPorcPlayer1(String value){
+        pplayer1.setText(value);
+    }
+    public void setPorcPlayer2(String value){
+        pplayer2.setText(value);
+    }
+    public JLabel getLabel1(){
+        return pplayer1;
+    }
+    public JLabel getLabel2(){
+        return pplayer2;
+    }
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton bt_novojg;
     private javax.swing.JButton bt_sairjg;
     private java.awt.Button button1;
     private java.awt.Button button10;
+    private java.awt.Button button11;
     private java.awt.Button button12;
     private java.awt.Button button2;
     private java.awt.Button button3;
     private java.awt.Button button4;
     private java.awt.Button button5;
     private java.awt.Button button6;
+    private java.awt.Button button7;
     private java.awt.Button button8;
     private java.awt.Button button9;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel12;
+    private javax.swing.JLabel jLabel13;
+    private javax.swing.JLabel jLabel14;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
-    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JPanel jPanel4;
+    private javax.swing.JLabel pplayer1;
+    private javax.swing.JLabel pplayer2;
     private javax.swing.JLabel vtr_1;
-    private javax.swing.JLabel vtr_2;
+    private javax.swing.JLabel vtr_3;
     // End of variables declaration//GEN-END:variables
 }
