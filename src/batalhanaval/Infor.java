0b8701983a9c4f193efae7fb3835edc31105a30b
Leonardo Santos <sleonardo144@gmail.com>
2020-11-12 12:40:43 -0200

    implementado alternação

--- src/batalhanaval/Infor.java
+++ src/batalhanaval/Infor.java
@@ -17,7 +17,7 @@ public class Infor extends javax.swing.JPanel {
     public Infor() {
         initComponents();
     }
-
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
